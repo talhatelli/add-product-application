69fc967899ca9d51b8d282ba2d9a85d4851c2767
talhatelli <ahmettalhatelli1453@gmail.com>
2022-12-09 01:14:31 +0300

    bind işlemleri componentlar arası iletişim yapıldı

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,52 @@
 <template>
   <div class="container">
-    <h1 class="text-center">Ürün Ekleme Uygulaması</h1>
-    <hr>
-    <appNewProduct></appNewProduct>
-    <br><br>
-    <appProgress></appProgress>
-    <br><br>
-    <h3 class="text-center">Eklenen Ürünlerin Listesi</h3>
-    <hr>
-    <appProducts></appProducts>
+      <h1 class="text-center">Ürün Ekleme Uygulaması</h1>
+      <hr>
+      <app-new-product></app-new-product>
+      <br><br>
+      <app-progress></app-progress>
+      <br><br>
+      <app-products></app-products>
   </div>
-
 </template>
 
 <script>
-import NewProduct from "./components/NewProduct";
-import Progress from "./components/Progress.vue";
-import Products from "./components/Products.vue";
+  import NewProduct from "./components/NewProduct";
+  import Progress from "./components/Progress";
+  import Products from "./components/Products";
 
-export default {
-  components: {
-    appNewProduct: NewProduct,
-    appProgress: Progress,
-    appProducts: Products
+  export default {
+      components: {
+          appNewProduct : NewProduct,
+          appProgress : Progress,
+          appProducts : Products,
+      },
 
-  },
-
-  data() {
-    return {
-      imageList: [],
-      product: {
-        selectedImage: null
-      }
-    }
-  },
-  methods: {
-    onChange(e) {
-      const file = e.target.files[0];
-      this.product.selectedImage = URL.createObjectURL(file);
-    }
   }
-}
 </script>
-
-
-
 <style>
-body {
-  background-color: aliceblue;
-  padding-top: 20px;
-  padding-bottom: 20px;
-}
+  body {
+      background-color: aliceblue;
+      padding-top: 20px;
+      padding-bottom: 20px;
+  }
 
-.card {
-  margin-right: 5px;
-  margin-bottom: 5px;
-}
+  .card {
+      margin-right: 5px;
+      margin-bottom: 5px;
+  }
 
-.card:last-child {
-  margin-right: 0px;
-}
+  .card:last-child {
+      margin-right: 0px;
+  }
 
-.col-md-2 {
-  max-width: 15.666667% !important;
-}
+  .col-md-2 {
+      max-width: 15.666667% !important;
+  }
 
-.product-container {
-  margin-left: 15px;
-}
+  .product-container {
+      margin-left: 15px;
+  }
+
 </style>
 
--- src/components/NewProduct.vue
+++ src/components/NewProduct.vue
@@ -3,7 +3,7 @@
         <div class="card offset-2 col-md-3">
             <div class="card-body tex-center d-flex align-items-center flex-column">
                 <img height="128" class="img-responsive text-center mb-3"
-                    :src="product.selectedImage == null ? '/src/assets/default.png' : product.selectedImage">
+                     :src="product.selectedImage == null ? '/src/assets/default.png' : product.selectedImage">
                 <input ref="file" type="file" style="display: none;" @change="onChange($event)" class="form-control">
                 <button class="btn btn-outline-secondary " type="button" @click="$refs.file.click()">Resim Seç</button>
             </div>
@@ -13,19 +13,21 @@
                 <div class="card-body">
                     <div class="form-group">
                         <label>Ürün Adı</label>
-                        <input type="text" class="form-control" placeholder="adını giriniz">
+                        <input type="text" v-model="product.title" class="form-control" placeholder="adını giriniz">
                     </div>
                     <div class="row">
                         <div class="form-group col-md-6">
                             <label>Ürün Adeti</label>
-                            <input type="text" class="form-control" placeholder="adetini giriniz">
+                            <input type="text" v-model="product.count" class="form-control"
+                                   placeholder="adetini giriniz">
                         </div>
                         <div class="form-group col-md-6">
                             <label>Ürün Fiyatı</label>
-                            <input type="text" class="form-control" placeholder="fiyatını giriniz">
+                            <input type="text" v-model="product.price" class="form-control"
+                                   placeholder="fiyatını giriniz">
                         </div>
                     </div>
-                    <button class="btn btn-outline-info btn-block">Ekle!</button>
+                    <button @click="addProduct" class="btn btn-outline-info btn-block">Ekle!</button>
                 </div>
             </div>
         </div>
@@ -33,19 +35,36 @@
 </template>
 
 <script>
+    import {eventBus} from "../main";
 
-export default {
-    data() {
-        return {
-            product: {
-
+    export default {
+        data() {
+            return {
+                product: {
+                    title: null,
+                    count: null,
+                    price: null,
+                    totalPrice: null,
+                    selectedImage: null
+                }
+            }
+        },
+        methods: {
+            onChange(e) {
+                const file = e.target.files[0];
+                this.product.selectedImage = URL.createObjectURL(file);
+            },
+            addProduct() {
+                this.product.totalPrice = this.product.count * this.product.price;
+                eventBus.$emit("productAdded", this.product);
+                this.product = {
+                    title: null,
+                    count: null,
+                    price: null,
+                    totalPrice: null,
+                    selectedImage: null
+                }
             }
         }
     }
-}
-
 </script>
-
-<style>
-
-</style>
