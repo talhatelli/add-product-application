2c0c07a0664d793efe4713976efc7f25ea11403f
talhatelli <ahmettalhatelli1453@gmail.com>
2022-12-08 19:47:59 +0300

    component laştırma

---
+++ src/components/Product.vue
@@ -0,0 +1,26 @@
+<template>
+      <div class="col-md-2 card">
+        <img class="card-img-top" src="/src/assets/default.png" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <small>
+            <strong>Adet : </strong> 1
+          </small>
+          <br>
+          <small>
+            <strong>Fiyat : </strong> 10
+          </small>
+          <br>
+          <small>
+            <strong>Tutar : </strong> 10
+          </small>
+        </div>
+      </div>
+</template>
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="row product-container">
+<appProduct></appProduct>
+    </div>
+</template>
+<script>
+import Product from "./Product.vue"
+export default{
+    components:{
+        appProduct:Product
+    }
+
+}
+</script>
+
+
+<style>
+
+</style>
